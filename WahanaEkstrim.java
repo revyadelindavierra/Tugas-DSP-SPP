578d8a6a8ece9739da6489a556f25828b199d521
revyadelindavierra <112836086+revyadelindavierra@users.noreply.github.com>
2022-10-03 12:21:39 +0700

    Add files via upload

---
+++ WahanaEkstrim.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Revy Adelinda
+ */
+public class WahanaEkstrim {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+int wahana;
+wahana = in.nextInt();
+        System.out.println("PILIH WAHANA ANDA:");
+        System.out.println();
+                }
+}
